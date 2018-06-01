ba71e958a63cd5bdc564d61d1032ca2406840c00
Perso Constant <constant.dupuis@gmail.com>
2018-06-01 14:21:35 +0200

    Add drawer with items, settings button

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,54 @@ class HomePage extends StatelessWidget{
      appBar: new AppBar(
       title: new Text('Erratic Master Remote'),
       elevation: 0.0,
+      actions: <Widget>[
+        new IconButton(
+          icon: new Icon(Icons.settings),
+          onPressed: (){
+            print('got to settings page');
+          },
+        ),
+      ],
+     ),
+     drawer: new Drawer(
+       child:new ListView(
+         children: <Widget>[
+           new Container(
+             color: Colors.orange,
+              child : new DrawerHeader(
+                child : new Text('Erratic Master Remote',
+                                  style: TextStyle(fontSize: 26.0),
+                                ),
+              ),
+           ),
+           new ListTile(
+             title: new Text('Overview'),
+             leading: new Icon(Icons.settings_input_antenna),
+             onTap: (){
+               print('Show Status page');
+             },
+           ),
+           new ListTile(
+             title: new Text('Show Control'),
+             leading: new Icon(Icons.settings_input_svideo),
+             onTap: (){
+               print('Show Show control page');
+             },
+           ),
+           new ListTile(
+             title: new Text('Hide'),
+             leading: new Icon(Icons.close),
+             onTap: (){
+               Navigator.of(context).pop();
+             },
+           ),
+         ],
+       ),
+     ),
+     body: new Container(
+       child: new Center(
+         child: new Text("Home Page"),
+       ),
      ),
    ) ;
   }
