3d07606e68dfa7cb4f3d0a42fcc09a768eff053f
Unknown <constant.dupuis@gmail.com>
2018-06-04 21:03:47 +0200

    try container around cue listTile

--- lib/showControl.dart
+++ lib/showControl.dart
@@ -10,14 +10,16 @@ class ShowControlPage extends StatelessWidget{
         ),
         drawer: new MyDrawer(),
         body: new Container(
-          padding: EdgeInsets.all(16.0),
           child: new ListView(
             children: <Widget>[
-              new ListTile(
-                title: new Text('CUE 01', style: TextStyle(fontSize: 26.0),),
-                trailing: new IconButton(
-                  icon: new Icon(Icons.play_circle_outline, size: 36.0, color: Colors.black,),
-                  onPressed: (){},
+              new Container(
+                padding: EdgeInsets.all(10.0),
+                child: new ListTile(
+                  title: new Text('CUE 01', style: TextStyle(fontSize: 26.0),),
+                  trailing: new IconButton(
+                    icon: new Icon(Icons.play_circle_outline, size: 36.0, color: Colors.black,),
+                    onPressed: (){},
+                  ),
                 ),
               ),
               new ListTile(
